a3e7d3ee5b98aad0a698972a6d2464a5dbb0726c
Claire Qiu <claireqch@gmail.com>
2021-02-10 18:55:00 -0500

    Init Code - part 2

--- src/nearest_neighbor_brute_force.cpp
+++ src/nearest_neighbor_brute_force.cpp
@@ -1,15 +1,26 @@
 #include "nearest_neighbor_brute_force.h"
 #include <limits>// std::numeric_limits<double>::infinity();
+#include <math.h> 
 
 void nearest_neighbor_brute_force(
-  const Eigen::MatrixXd & points,
-  const Eigen::RowVector3d & query,
-  int & I,
-  double & sqrD)
+    const Eigen::MatrixXd& points,
+    const Eigen::RowVector3d& query,
+    int& I,
+    double& sqrD)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  I = -1;
-  sqrD = 0;
-  ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    // Replace with your code here:
+    double nearest_dis = std::numeric_limits<double>::infinity();
+    double nearest_I = -1;
+    for (int i = 0; i < points.rows(); i++) {
+        double dis_current = pow(query[0] - points(i, 0), 2) + pow(query[1] - points(i, 1), 2) + pow(query[2] - points(i, 2), 2);
+        if (dis_current < nearest_dis) {
+            nearest_dis = dis_current;
+            nearest_I = i;
+        }
+    }
+    I = nearest_I;
+    sqrD = nearest_dis;
+
+    ////////////////////////////////////////////////////////////////////////////
 }
--- src/point_box_squared_distance.cpp
+++ src/point_box_squared_distance.cpp
@@ -1,11 +1,16 @@
 #include "point_box_squared_distance.h"
+#include <math.h> 
 
 double point_box_squared_distance(
-  const Eigen::RowVector3d & query,
-  const BoundingBox & box)
+	const Eigen::RowVector3d& query,
+	const BoundingBox& box)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here
-  return 0;
-  ////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	// Replace with your code here
+	double x = std::max( std::max(box.min_corner(0) - query(0), query(0) - box.max_corner(0)), 0.0 );
+	double y = std::max( std::max(box.min_corner(1) - query(1), query(1) - box.max_corner(1)), 0.0 );
+	double z = std::max( std::max(box.min_corner(2) - query(2), query(2) - box.max_corner(2)), 0.0 );
+
+	return pow(x, 2) + pow(y, 2) + pow(z, 2);
+	////////////////////////////////////////////////////////////////////////////
 }
